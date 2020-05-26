b917247ff32959ae5f7931750600eae3e974b8bf
annagrinkotest <64903335+annagrinkotest@users.noreply.github.com>
2020-05-26 11:24:00 +0300

    Add file without jquery

--- main.js
+++ main.js
@@ -6,70 +6,71 @@ document.addEventListener('DOMContentLoaded', () => {
     var rectWidth = 50;
     var rectHeight = 50;
    
-	  
-	    $("#x1-plus").click(function(){
+	    document.querySelector("#x1-plus").addEventListener("click", () => { 
+	    
 		    if (valueXFirst>=10 && valueXFirst<=580 && rectWidth>10 )
 		        {valueXFirst+=10;
 				rectWidth=rectWidth-10;
-				$('#x1').text(valueXFirst);
-				$('.rect').css("left",valueXFirst);			
-				$('.rect').css("width",rectWidth);}
+				document.querySelector("#x1").textContent = valueXFirst;
+				document.querySelector(".rect").style.left = valueXFirst;
+				document.querySelector(".rect").style.width = rectWidth;
+			}
 			  
 			});
 		
 		
-	    $("#x1-minus").click(function(){
-			if (valueXFirst> 10 && valueXFirst<=580 && rectWidth>=10 ){
+	    document.querySelector("#x1-minus").addEventListener("click",() =>{
+			if (valueXFirst> 10 && valueXFirst<=590 && rectWidth>=10 ){
 				valueXFirst-=10;
 				rectWidth=rectWidth+10;		
-				$('#x1').text(valueXFirst);        
-				$('.rect').css("width",rectWidth);
-				$('.rect').css("left",valueXFirst);}
+				document.querySelector("#x1").textContent = valueXFirst;       
+				document.querySelector(".rect").style.left = valueXFirst;
+				document.querySelector(".rect").style.width = rectWidth;}
 			
 		    });
-		$("#y1-plus").click(function(){
+		document.querySelector("#y1-plus").addEventListener("click",() =>{
 			 if (valueYFirst>=10 && valueYFirst<=480 && rectHeight>10 ){
 				valueYFirst+=10;
 			    rectHeight=rectHeight-10;
-			    $('#y1').text(valueYFirst);
-				$('.rect').css("top", valueYFirst);
-				$('.rect').css("height", rectHeight);}
+			    document.querySelector('#y1').textContent = valueYFirst;
+				document.querySelector('.rect').style.top = valueYFirst;
+				document.querySelector('.rect').style.height = rectHeight;}
 		});
-	    $("#y1-minus").click(function(){
-			if (valueYFirst>10 && valueYFirst<=480 && rectHeight>=10 ){
+	    document.querySelector("#y1-minus").addEventListener("click",() =>{
+			if (valueYFirst>10 && valueYFirst<=490 && rectHeight>=10 ){
 				valueYFirst-=10;
 				rectHeight=rectHeight+10;
-				$('#y1').text(valueYFirst);
-				$('.rect').css("top", valueYFirst);
-				$('.rect').css("height", rectHeight);}
+				document.querySelector('#y1').textContent = valueYFirst;
+				document.querySelector('.rect').style.top = valueYFirst;
+				document.querySelector('.rect').style.height = rectHeight;}
 		});
-		$("#x2-plus").click(function(){
+		document.querySelector("#x2-plus").addEventListener("click",() =>{
 			if (valueXSecond>=10 && valueXSecond<=580 && rectWidth>=10 ){
 				valueXSecond+=10;
 				rectWidth=rectWidth+10;
-				$('#x2').text(valueXSecond);
-				$('.rect').css("width", rectWidth);}
+				document.querySelector('#x2').textContent = valueXSecond;
+				document.querySelector('.rect').style.width = rectWidth;}
 		});
-	    $("#x2-minus").click(function(){
-			if (valueXSecond>=10 && valueXSecond<=580 && rectWidth>10 ){
+	    document.querySelector("#x2-minus").addEventListener("click",() =>{
+			if (valueXSecond>=10 && valueXSecond<=590 && rectWidth>10 ){
 				valueXSecond-=10;
 				rectWidth=rectWidth-10;
-				$('#x2').text(valueXSecond);
-				$('.rect').css("width", rectWidth);}
+				document.querySelector('#x2').textContent = valueXSecond;
+				document.querySelector('.rect').style.width = rectWidth;}
 		});
-		$("#y2-plus").click(function(){
+		document.querySelector("#y2-plus").addEventListener("click",()=> {
 			if (valueYSecond>=10 && valueYSecond<=480 && rectHeight>=10 ){
 				valueYSecond+=10;
 				rectHeight=rectHeight+10;
-				$('#y2').text(valueYSecond);
-				$('.rect').css("height", rectHeight);}
+				document.querySelector('#y2').textContent = valueYSecond;
+				document.querySelector('.rect').style.height = rectHeight;}
 		});
-	    $("#y2-minus").click(function(){
-			if (valueYSecond>10 && valueYSecond<=480 && rectHeight>10 ){
+	    document.querySelector("#y2-minus").addEventListener("click",()=>{
+			if (valueYSecond>10 && valueYSecond<=490 && rectHeight>10 ){
 				valueYSecond-=10;
 				rectHeight=rectHeight-10;
-				$('#y2').text(valueYSecond);
-				$('.rect').css("height", rectHeight);}
+				document.querySelector('#y2').textContent = valueYSecond;
+				document.querySelector('.rect').style.height = rectHeight;}
 	});
 		
 });
